aaa4305740578ad6de8310dc5408774b6f21a14b
Savannah <savmaxwell@gmail.com>
2022-03-08 11:19:02 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,7 +14,18 @@ router.get('/', (req, res) => {
         })
 })
 router.get('/:id', (req, res) => {
-    console.log('get post with id from posts router')
+    const id = req.params.id;
+    Post.findById(id)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The post information could not be retrieved"})
+        })
 })
 router.post('/', (req, res) => {
     console.log('add new post from posts router')
